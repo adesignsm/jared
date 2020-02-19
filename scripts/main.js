94376d3dfada448cc6ab3b77f751d47ca8782b09
freakolope <akashmulye97@gmail.com>
2020-02-19 18:23:34 -0500

    initial

--- scripts/main.js
+++ scripts/main.js
@@ -20,3 +20,34 @@ $.smartscroll({
     headerHash: "header"
 });
 
+setTimeout(function() {
+
+    $(window).scroll(function() {
+
+        if (window.location.hash.indexOf("#section1") > -1 ) {
+            $("#nav-bar-fill").stop().animate({height: "25%"}, 200);
+            $("#section1-details-text h1").delay(600).animate({marginLeft: "0", opacity: "1"}, 1000);
+            $("#view-button").stop().animate({right: "10%", opacity: "1"}, 700);
+
+        } else if (window.location.hash.indexOf("#section2") > -1) { 
+            $("#nav-bar-fill").stop().animate({height: "50%"}, 200);
+            $("#section2-details-text h1").delay(600).animate({marginLeft: "0", opacity: "1"}, 1000);
+
+        } else if (window.location.hash.indexOf("#section3") > -1) {
+            $("#nav-bar-fill").stop().animate({height: "75%"}, 200);
+            $("#section3-details-text h1").delay(600).animate({marginLeft: "0", opacity: "1"}, 1000);
+
+        } else if (window.location.hash.indexOf("#section4") > -1) {
+            $("#nav-bar-fill").stop().animate({height: "100%"}, 200);
+            $("#section4-details-text h1").delay(600).animate({marginLeft: "0", opacity: "1"}, 1000);
+
+        } else if (window.location.hash.indexOf("#header") > -1) {
+            $("#nav-bar-fill").stop().animate({height: "0%"}, 200); 
+            $("#view-button").stop().animate({right: "0%", opacity: "0"}, 100);      
+        }
+    });
+
+}, 200);
+
+
+
